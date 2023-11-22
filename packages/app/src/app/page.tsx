69f8fb4191261f033769abe9156fbd940a12047d
Stevan Bogosavljevic <stevyhacker@gmail.com>
2023-11-22 22:04:01 +0100

    Implement creating a pool and adding liquidity

--- packages/app/src/app/page.tsx
+++ packages/app/src/app/page.tsx
@@ -10,15 +10,32 @@ export default function Home() {
       <div>
         <p>
           UniswapV3Factory
-          <br />
-          0x41fcE16a0Ccc749e3036F62f42a838c176A0D70F
+          <a
+            className={'text-blue-400 underline'}
+            href={'https://goerli.explorer.zksync.io/address/0x41fcE16a0Ccc749e3036F62f42a838c176A0D70F'}>
+            <br />
+            0x41fcE16a0Ccc749e3036F62f42a838c176A0D70F
+          </a>
         </p>
         <br />
-
         <p>
           SwapRouter02
-          <br />
-          0x0BBDfB697D515EF154628d659724C58d8e0D5CDC
+          <a
+            className={'text-blue-400 underline'}
+            href={'https://goerli.explorer.zksync.io/address/0x0BBDfB697D515EF154628d659724C58d8e0D5CDC'}>
+            <br />
+            0x0BBDfB697D515EF154628d659724C58d8e0D5CDC
+          </a>
+        </p>
+        <br />
+        <p>
+          NonfungiblePositionManager
+          <a
+            className={'text-blue-400 underline'}
+            href={'https://goerli.explorer.zksync.io/address/0xA87AA5CF925542436872207ffcDbFF792f388EA5'}>
+            <br />
+            0xA87AA5CF925542436872207ffcDbFF792f388EA5
+          </a>
         </p>
       </div>
 
